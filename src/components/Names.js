0c7809ad830ad499d07981b8cf0707bb55c26547
Agne Nevulyte <agne.nevulyte@finsaeurope.com>
2019-09-25 13:17:42 +0100

    Changed Names class into function,receiving props

--- src/components/Names.js
+++ src/components/Names.js
@@ -1,37 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Names extends Component {
-  constructor() {
-    super();
-    this.state = {
-      names: []
-    };
-  }
-
-  componentDidMount = () => {
-    this.fetchNames();
-  };
-
-  fetchNames = () => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(res => res.json())
-      .then(json => {
-        console.log("json", json);
-        this.setState({ names: json });
-      })
-      .catch(err => alert(err));
-  };
-  render() {
-    console.log(this.state.names);
-    return (
-      <div>
-        <h1>Names:</h1>
-        <ul>
-          {this.state.names.map((each, i) => {
-            return <li key={i}>{each.name}</li>;
-          })}
-        </ul>
-      </div>
-    );
-  }
+export default function Names(props) {
+  console.log(props.names);
+  return (
+    <div>
+      <h1>Names:</h1>
+      <ul></ul>
+    </div>
+  );
 }
