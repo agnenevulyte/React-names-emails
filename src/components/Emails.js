5feac939dc7f592895a458eb6be5c9962d7c09a2
Agne Nevulyte <agne.nevulyte@finsaeurope.com>
2019-09-25 14:27:11 +0100

    mapping through Names and Emails

--- src/components/Emails.js
+++ src/components/Emails.js
@@ -2,11 +2,15 @@ import React, { Component } from "react";
 
 export default class Emails extends Component {
   render() {
-    console.log(this.props.emails);
+    // console.log(this.props.emails);
     return (
       <div>
         <h1>Emails:</h1>
-        <ul></ul>
+        <ul>
+          {this.props.emails.map((each, i) => {
+            return <li key={i}>{each}</li>;
+          })}
+        </ul>
       </div>
     );
   }
--- src/components/Names.js
+++ src/components/Names.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 export default function Names(props) {
-  console.log(props.names);
+  //   console.log(props.names);
   return (
     <div>
       <h1>Names:</h1>
-      <ul></ul>
+      <ul>
+        {props.names.map((each, i) => {
+          return <li key={i}>{each}</li>;
+        })}
+      </ul>
     </div>
   );
 }
