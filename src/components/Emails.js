fa8f112c54e512ee052e8f7441c0fd1407228167
Agne Nevulyte <agne.nevulyte@finsaeurope.com>
2019-09-25 13:18:24 +0100

    Emails class receiving this.props

--- src/components/Emails.js
+++ src/components/Emails.js
@@ -1,37 +1,12 @@
 import React, { Component } from "react";
-import { componentDidMount } from "./Names";
 
 export default class Emails extends Component {
-  constructor() {
-    super();
-    this.state = {
-      emails: []
-    };
-  }
-
-  componentDidMount = () => {
-    this.fetchEmails();
-  };
-
-  fetchEmails = () => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(res => res.json())
-      .then(json => {
-        console.log("json", json);
-        this.setState({ emails: json });
-      })
-      .catch(err => alert(err));
-  };
-
   render() {
+    console.log(this.props.emails);
     return (
       <div>
         <h1>Emails:</h1>
-        <ul>
-          {this.state.emails.map((each, i) => {
-            return <li key={i}>{each.email}</li>;
-          })}
-        </ul>
+        <ul></ul>
       </div>
     );
   }
